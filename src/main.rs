63b12b820e949571193b910279bed7c2a1eec24c
KoblizekXD <janprokupek@centrum.cz>
2023-06-23 16:53:01 +0200

    added: initial files

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// https://maven.minecraftforge.net/net/minecraftforge/forge/<version>/forge-<version>-installer.jar
+// https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.2/fabric-installer-0.11.2.jar
+
+fn main() {
+
+}
